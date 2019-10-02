cdd3082c58c0fc25831579fcdd8b0ebbb33cc93e
PedroAngeli <pedroans.angeli@hotmail.com>
2019-10-01 21:23:55 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <sys/types.h>
+
+int main(){
+
+  while(1){
+    printf("gsh> ");
+    char cmd[100];
+    scanf("%[^\n]%*c",cmd);
+    
+    pid_t pid = fork();
+
+    if(pid == 0){
+      system(cmd);
+    }
+  }
+
+  return 0;
+}
